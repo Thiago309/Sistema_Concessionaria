ef278c495ec411b45983445bf5efefa40687a683
Gusta-0 <gabs.principal.2005@gmail.com>
2024-11-05 13:58:19 -0300

    ggg

--- POO/Concessionaria/src/main/java/cliente/Cliente.java
+++ POO/Concessionaria/src/main/java/cliente/Cliente.java
@@ -2,10 +2,14 @@ package cliente;
 
 public class Cliente {
     protected String idCliente;
+    protected String nomeCliente;
     protected String cep;
     protected String logradouro;
-    protected int numero;
-    protected String complemento;
+    protected int telefone;
+    protected String Estado;
+    protected String Cidade;
+    protected String Email;
+
 
     public Cliente() {
 
@@ -15,27 +19,11 @@ public class Cliente {
         this.idCliente = idCliente;
         this.cep = cep;
         this.logradouro = logradouro;
-        this.numero = numero;
-        this.complemento = complemento;
+        this.telefone = telefone;
+
     }
 }
-
-    /*
-    private String idCliente;
-    private String nome;
-    private String cep;
-    private String logradouro;
-    private int numero;
-    private String complemento;
-
-    public Cliente(String idCliente, String cep, String logradouro, int numero, String complemento) {
-        this.idCliente = idCliente;
-        this.cep = cep;
-        this.logradouro = logradouro;
-        this.numero = numero;
-        this.complemento = complemento;
-    }
-
+/*
     // Getters e Setters
     public String getIdCliente() { return idCliente; }
     public void setIdCliente(String idCliente) { this.idCliente = idCliente; }
